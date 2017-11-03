dfbab450820031d0943e4d6f830da6c31c699a92
chantal15 <chantalsaldivar530@gmail.com>
2017-11-03 09:44:36 +0800

    For imports and data attributes

---
+++ Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/Insert_Session.java
@@ -0,0 +1,37 @@
+package ispector.edu.apc.async;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import ispector.edu.apc.global.HousekeepingApp;
+import ispector.edu.apc.global.Logs;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by ispector on 2017-10-25.
+ */
+
+public class Insert_Session extends AsyncTask<Void, Void, String> {
+
+    private MyInterface mListener;
+    private String TAG = Insert_Session.class.getSimpleName()
+            , _userType
+            , _roomNo
+            , _userNo
+            , _HK_START = null
+            , _HK_END = null
+            , _INSP_START = null
+            , _INSP_END = null
+            ;
+    private Context _context;
+
+
