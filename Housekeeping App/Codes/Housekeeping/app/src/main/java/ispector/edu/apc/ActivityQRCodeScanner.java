04679ca2248d9775ad41636838ef6795e2669d9d
kmbarrion <barrion.kevin@gmail.com>
2017-11-03 10:31:31 +0800

    codes to Programmatically initialize the scanner view and Set the scanner view as the content view

--- Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/ActivityQRCodeScanner.java
+++ Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/ActivityQRCodeScanner.java
@@ -13,4 +13,15 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
  * Created by ispector on 10/17/2017.
  */
 
-   
+    public class ActivityQRCodeScanner extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    private String TAG = ActivityQRCodeScanner.class.getSimpleName();
+    @Override
+    public void onCreate(Bundle state) {
+            super.onCreate(state);
+            mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+            setContentView(mScannerView);                // Set the scanner view as the content view
+            }
+
+  
