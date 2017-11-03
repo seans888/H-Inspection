370d3b1c5ac11185c5ff5397c70c9042fd6190da
kmbarrion <barrion.kevin@gmail.com>
2017-11-03 10:30:17 +0800

    insert imports for qrcode scanning

---
+++ Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/ActivityQRCodeScanner.java
@@ -0,0 +1,16 @@
+package ispector.edu.apc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.zxing.Result;
+import ispector.edu.apc.global.HousekeepingApp;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+/**
+ * Created by ispector on 10/17/2017.
+ */
+
+   
