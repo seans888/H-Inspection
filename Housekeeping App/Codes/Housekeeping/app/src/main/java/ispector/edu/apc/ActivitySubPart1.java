1c4554ae18949a9e62f743bafe918436c44f277b
kmbarrion <barrion.kevin@gmail.com>
2017-11-03 11:04:38 +0800

    activity for inspection checklist part 1

---
+++ Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/ActivitySubPart1.java
@@ -0,0 +1,13 @@
+package ispector.edu.apc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitySubPart1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_part1);
+    }
+}
