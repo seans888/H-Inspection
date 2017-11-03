42810c1ed8d4a7877faa9b7068adc509ad971e88
chantal15 <chantalsaldivar530@gmail.com>
2017-11-03 10:43:06 +0800

    attributes for inspection checklist

--- Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/ActivityRoomSelection.java
+++ Housekeeping App/Codes/Housekeeping/app/src/main/java/ispector/edu/apc/ActivityRoomSelection.java
@@ -24,3 +24,38 @@ import ispector.edu.apc.hardware.CameraState;
 import java.util.ArrayList;
 import java.util.List;
 
+public class ActivityRoomSelection extends AppCompatActivity {
+
+    private Context c;
+    private TextView tv_UserFullName
+            , tv_UserType
+            , tv_RoomStatus;
+    private Spinner spnr_RoomNo;
+    private Button btn_StartQR
+            , btn_EndQR;
+    private String logInResult
+            , Fullname
+            , UserType
+            , TAG = ActivityRoomSelection.class.getSimpleName()
+            , token;
+    private RadioButton rb_Suite
+            , rb_NonSuite;
+    private RadioGroup rg_RoomType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_selection);
+
+        c = this;
+
+        tv_UserFullName = (TextView) findViewById(R.id.tv_UserFullName);
+        tv_UserType = (TextView) findViewById(R.id.tv_UserType);
+        tv_RoomStatus = (TextView) findViewById(R.id.tv_RoomStatus);
+        rb_Suite = (RadioButton) findViewById(R.id.rb_Suite);
+        rb_NonSuite = (RadioButton) findViewById(R.id.rb_NonSuite);
+        rg_RoomType = (RadioGroup) findViewById(R.id.rg_RoomType);
+        spnr_RoomNo = (Spinner) findViewById(R.id.spnr_RoomNo);
+        btn_StartQR = (Button) findViewById(R.id.btn_StartQR);
+        btn_EndQR = (Button) findViewById(R.id.btn_EndQR);
+
