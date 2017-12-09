54683eaea69cc6ad3526a9eda737baeaa5f8eb3b
gdgonzalez <gabdgonzalez@gmail.com>
2017-12-09 17:17:53 +0800

    Set variables for Intent result used for qr code

---
+++ Ispect/app/src/main/java/com/chanty/ispector/IntentResult.java
@@ -0,0 +1,11 @@
+package com.chanty.ispector;
+
+
+public final class IntentResult {
+
+    private final String contents;
+    private final String formatName;
+    private final byte[] rawBytes;
+    private final Integer orientation;
+    private final String errorCorrectionLevel;
+
