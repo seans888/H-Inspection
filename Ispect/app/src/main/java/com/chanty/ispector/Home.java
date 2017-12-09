20426b2ffb5b08b6924f6733b5c98080ea9af003
gdgonzalez <gabdgonzalez@gmail.com>
2017-12-09 10:45:42 +0800

    Navigation view gets  from nav menu layout

--- Ispect/app/src/main/java/com/chanty/ispector/Home.java
+++ Ispect/app/src/main/java/com/chanty/ispector/Home.java
@@ -28,3 +28,56 @@ public class Home extends AppCompatActivity {
         //NAVIGATION DRAWER
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+
+
+ getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//NAVIGATION VIEW GETS FROM NAV_MENU LAYOUT
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_menu);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override public boolean onNavigationItemSelected(MenuItem menuItem)
+            { switch (menuItem.getItemId())
+            {
+                case(R.id.nav_home): Intent accountActivity = new Intent(getApplicationContext(), Home.class);
+                    startActivity(accountActivity);
+                    finish();
+                    break;
+
+                case(R.id.nav_logout): Intent accountActivity4 = new Intent(getApplicationContext(), Registration.class);
+                    startActivity(accountActivity4);
+                    Toast.makeText(getApplicationContext(), "Signed out!",
+                            Toast.LENGTH_LONG).show();
+                    FirebaseAuth.getInstance().signOut();
+                    break;
+
+                case(R.id.nav_suite): Intent accountActivity5 = new Intent(getApplicationContext(), ScannerParent1.class);
+                    Intent intent1 = getIntent();
+                    String userEmail1 = intent1.getStringExtra("USER_EMAIL");
+                    String userKey1 = intent1.getStringExtra("USER_KEY");
+                    String userType1 = intent1.getStringExtra("USER_TYPE");
+                    accountActivity5.putExtra("userEmail", userEmail1);
+                    accountActivity5.putExtra("userKey", userKey1);
+                    accountActivity5.putExtra("userType", userType1);
+                    startActivity(accountActivity5);
+                    finish();
+                    break;
+
+                case(R.id.nav_normal): Intent accountActivity6 = new Intent(getApplicationContext(), ScannerParent.class);
+                    Intent intent = getIntent();
+                    String userEmail = intent.getStringExtra("USER_EMAIL");
+                    String userKey = intent.getStringExtra("USER_KEY");
+                    String userType = intent.getStringExtra("USER_TYPE");
+                    accountActivity6.putExtra("userEmail", userEmail);
+                    accountActivity6.putExtra("userKey", userKey);
+                    accountActivity6.putExtra("userType", userType);
+                    startActivity(accountActivity6);
+                    finish();
+                    break;
+
+
+            }
+                return true;
+            } });
+
+        //NAVIGATION DRAWER
+    }
