4b145eddc336ea0a01f41f4848ca2216e7b02cbb
kmbarrion <barrion.kevin@gmail.com>
2017-12-09 18:05:36 +0800

    other components of intent Integrator

--- Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
+++ Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
@@ -246,4 +246,38 @@ public IntentIntegrator(Activity activity) {
             }
         }
         return false;
+    }
+    
+     private AlertDialog showDownloadDialog() {
+        AlertDialog.Builder downloadDialog = new AlertDialog.Builder(activity);
+        downloadDialog.setTitle(title);
+        downloadDialog.setMessage(message);
+        downloadDialog.setPositiveButton(buttonYes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String packageName;
+                if (targetApplications.contains(BS_PACKAGE)) {
+                    // Prefer to suggest download of BS if it's anywhere in the list
+                    packageName = BS_PACKAGE;
+                } else {
+                    // Otherwise, first option:
+                    packageName = targetApplications.get(0);
+                }
+                Uri uri = Uri.parse("market://details?id=" + packageName);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    if (fragment == null) {
+                        activity.startActivity(intent);
+                    } else {
+                        fragment.startActivity(intent);
+                    }
+                } catch (ActivityNotFoundException anfe) {
+                    // Hmm, market is not installed
+                    Log.w(TAG, "Google Play is not installed; cannot install " + packageName);
+                }
+            }
+        });
+        downloadDialog.setNegativeButton(buttonNo, null);
+        downloadDialog.setCancelable(true);
+        return downloadDialog.show();
     }    
