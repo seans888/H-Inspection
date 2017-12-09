8df7cee41eb70155b87a7c15ed0aa2d5e046e0d7
chantal15 <chantalsaldivar530@gmail.com>
2017-12-09 17:36:20 +0800

    QR Code format

--- Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
+++ Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
@@ -22,3 +22,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+public class IntentIntegrator {
+
+    public static final int REQUEST_CODE = 0x0000c0de; // Only use bottom 16 bits
+    private static final String TAG = IntentIntegrator.class.getSimpleName();
+
+    public static final String DEFAULT_TITLE = "Install Barcode Scanner?";
+    public static final String DEFAULT_MESSAGE =
+            "This application requires Barcode Scanner. Would you like to install it?";
+    public static final String DEFAULT_YES = "Yes";
+    public static final String DEFAULT_NO = "No";
+
+    private static final String BS_PACKAGE = "com.google.zxing.client.android";
+    private static final String BSPLUS_PACKAGE = "com.srowen.bs.android";
+
+    // supported barcode formats
+    public static final Collection<String> PRODUCT_CODE_TYPES = list("UPC_A", "UPC_E", "EAN_8", "EAN_13", "RSS_14");
+    public static final Collection<String> ONE_D_CODE_TYPES =
+            list("UPC_A", "UPC_E", "EAN_8", "EAN_13", "CODE_39", "CODE_93", "CODE_128",
+                    "ITF", "RSS_14", "RSS_EXPANDED");
+    public static final Collection<String> QR_CODE_TYPES = Collections.singleton("QR_CODE");
+    public static final Collection<String> DATA_MATRIX_TYPES = Collections.singleton("DATA_MATRIX");
+
+    public static final Collection<String> ALL_CODE_TYPES = null;
+
+    public static final List<String> TARGET_BARCODE_SCANNER_ONLY = Collections.singletonList(BS_PACKAGE);
+    public static final List<String> TARGET_ALL_KNOWN = list(
+            BSPLUS_PACKAGE,             // Barcode Scanner+
+            BSPLUS_PACKAGE + ".simple", // Barcode Scanner+ Simple
+            BS_PACKAGE                  // Barcode Scanner
+            // What else supports this intent?
+    );
