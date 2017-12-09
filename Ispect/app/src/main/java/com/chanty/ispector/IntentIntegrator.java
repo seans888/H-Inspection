0caa9a6155628dcdad8b4a33aa67dd46d45cb276
kmbarrion <barrion.kevin@gmail.com>
2017-12-09 18:02:34 +0800

    for checking which type of code to scan for qr code scanning

--- Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
+++ Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
@@ -179,3 +179,71 @@ public IntentIntegrator(Activity activity) {
     public final AlertDialog initiateScan(Collection<String> desiredBarcodeFormats, int cameraId) {
         Intent intentScan = new Intent(BS_PACKAGE + ".SCAN");
         intentScan.addCategory(Intent.CATEGORY_DEFAULT);
+
+    // check which types of codes to scan for
+        if (desiredBarcodeFormats != null) {
+            // set the desired barcode types
+            StringBuilder joinedByComma = new StringBuilder();
+            for (String format : desiredBarcodeFormats) {
+                if (joinedByComma.length() > 0) {
+                    joinedByComma.append(',');
+                }
+                joinedByComma.append(format);
+            }
+            intentScan.putExtra("SCAN_FORMATS", joinedByComma.toString());
+        }
+
+        // check requested camera ID
+        if (cameraId >= 0) {
+            intentScan.putExtra("SCAN_CAMERA_ID", cameraId);
+        }
+
+        String targetAppPackage = findTargetAppPackage(intentScan);
+        if (targetAppPackage == null) {
+            return showDownloadDialog();
+        }
+        intentScan.setPackage(targetAppPackage);
+        intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intentScan.addFlags(FLAG_NEW_DOC);
+        attachMoreExtras(intentScan);
+        startActivityForResult(intentScan, REQUEST_CODE);//REQUEST_CODE
+
+
+
+
+        return null;
+    }
+
+
+
+
+    protected void startActivityForResult(Intent intent, int code) {
+        if (fragment == null) {
+            activity.startActivityForResult(intent, code);
+        } else {
+            fragment.startActivityForResult(intent, code);
+        }
+    }
+
+    private String findTargetAppPackage(Intent intent) {
+        PackageManager pm = activity.getPackageManager();
+        List<ResolveInfo> availableApps = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (availableApps != null) {
+            for (String targetApp : targetApplications) {
+                if (contains(availableApps, targetApp)) {
+                    return targetApp;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static boolean contains(Iterable<ResolveInfo> availableApps, String targetApp) {
+        for (ResolveInfo availableApp : availableApps) {
+            String packageName = availableApp.activityInfo.packageName;
+            if (targetApp.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
+    }    
