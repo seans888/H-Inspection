ca475b3254c0a3095245bffcac005651a364d751
gdgonzalez <gabdgonzalez@gmail.com>
2017-12-09 17:20:40 +0800

    Intent Result components

--- Ispect/app/src/main/java/com/chanty/ispector/IntentResult.java
+++ Ispect/app/src/main/java/com/chanty/ispector/IntentResult.java
@@ -9,3 +9,18 @@ public final class IntentResult {
     private final Integer orientation;
     private final String errorCorrectionLevel;
 
+IntentResult() {
+        this(null, null, null, null, null);
+    }
+
+    IntentResult(String contents,
+                 String formatName,
+                 byte[] rawBytes,
+                 Integer orientation,
+                 String errorCorrectionLevel) {
+        this.contents = contents;
+        this.formatName = formatName;
+        this.rawBytes = rawBytes;
+        this.orientation = orientation;
+        this.errorCorrectionLevel = errorCorrectionLevel;
+    }
