2299255528022538b5ca7c5db20fe540ee93fe6b
gdgonzalez <gabdgonzalez@gmail.com>
2017-12-09 17:30:50 +0800

    Imports for Intent Integrator

---
+++ Ispect/app/src/main/java/com/chanty/ispector/IntentIntegrator.java
@@ -0,0 +1,24 @@
+package com.chanty.ispector;
+
+
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
