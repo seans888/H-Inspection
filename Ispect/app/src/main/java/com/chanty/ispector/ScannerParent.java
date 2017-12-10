4c6c3af50311a0733efdde867e808285ff093278
gdgonzalez <gabdgonzalez@gmail.com>
2017-12-10 12:55:08 +0800

    Declared variables from xml, get instance and reference from database

--- Ispect/app/src/main/java/com/chanty/ispector/ScannerParent.java
+++ Ispect/app/src/main/java/com/chanty/ispector/ScannerParent.java
@@ -23,3 +23,33 @@ import com.google.firebase.database.ValueEventListener;
 
 public class ScannerParent extends AppCompatActivity {
 
+
+//INITIALIZE VARIABLES FROM XML
+    private Button room1, room2, room3, room4, room5, room6, room7, room8, room9,room10;
+    private DatabaseReference mFirebaseDatabase, mFirebaseDatabase1, userRef;
+    private FirebaseDatabase mFirebaseInstance;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private  String roomStat;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_parent);
+        final boolean finish = getIntent().getBooleanExtra("finish", false);
+        if (finish) {
+            startActivity(new Intent(this, Login.class));
+            finish();
+            return;
+        }
+
+
+        //GET INSTANCE AND REFERENCES FROM DATABASE
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase1 = mFirebaseInstance.getReference("Rooms");
+        mFirebaseDatabase = mFirebaseInstance.getReference("Users");
