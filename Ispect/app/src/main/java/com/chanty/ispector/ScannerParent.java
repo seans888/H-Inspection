ec30a0bae8b8d06757dd0d045c89f28bd8a06685
gdgonzalez <gabdgonzalez@gmail.com>
2017-12-10 12:51:23 +0800

    Imports added for normal room's button and other funtionalities

---
+++ Ispect/app/src/main/java/com/chanty/ispector/ScannerParent.java
@@ -0,0 +1,25 @@
+package com.chanty.ispector;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ScannerParent extends AppCompatActivity {
+
