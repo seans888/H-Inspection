bd376df47f52747bb112e576bfd16076b4c1f4f2
chantal15 <chantalsaldivar530@gmail.com>
2017-12-10 15:40:10 +0800

    Scanner yellow class for suite room added. with if and else statement and firebase connection

---
+++ Ispect/app/src/main/java/com/chanty/ispector/ScannerYellow1.java
@@ -0,0 +1,113 @@
+package com.chanty.ispector;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ScannerYellow1 extends AppCompatActivity {
+    private Button scan_btn;
+    private FirebaseDatabase mFirebaseInstance;
+    private DatabaseReference mFirebaseDatabase1, mFirebaseDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_yellow);
+
+
+        mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase1 = mFirebaseInstance.getReference("Rooms");
+        mFirebaseDatabase = mFirebaseInstance.getReference("Users");
+        scan_btn = (Button) findViewById(R.id.scan_btn);
+        final Activity activity = this;
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+
+
+
+
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents()==null){
+                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+            }
+            else {
+
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                Intent inte = getIntent();
+                String RoomName = inte.getStringExtra("RoomName");
+                String userEmail = inte.getStringExtra("userEmail");
+                String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+                DatabaseReference reference = mFirebaseDatabase1.child(RoomName).child(date).child("Sign-out").child(currentDateTimeString).child("Room");;
+                DatabaseReference reference1 = mFirebaseDatabase1.child(RoomName).child(date).child("Sign-out").child(currentDateTimeString).child("User");
+                reference1.setValue(userEmail);
+                String resultnew = result.toString();
+                reference.setValue(resultnew);
+
+                Toast.makeText(getApplicationContext(), "Ready for inspection!!",
+                        Toast.LENGTH_LONG).show();
+
+                Intent mIntent = new Intent(getApplicationContext(), ScannerParent.class);
+                mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(mIntent);
+                finish();
+
+            }
+
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+
+
+        }
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        String message = "BEING HOUSEKEPT";
+        Intent inte = getIntent();
+        String RoomName = inte.getStringExtra("RoomName");
+        DatabaseReference reference = mFirebaseDatabase1.child(RoomName).child("RoomStatus");
+        reference.setValue(message);
+        Intent mIntent = new Intent(getApplicationContext(), ScannerParent.class);
+        startActivity(mIntent);
+        finish();
+    }
+}
